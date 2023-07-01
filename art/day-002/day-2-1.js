2b9bca645f01d2ba706d3cf9a0576b3e8d9637f5
Athoug <athoug.s@gmail.com>
2023-07-01 10:54:19 +0300

    day 2 complete
    three different versions

---
+++ art/day-002/day-2-1.js
@@ -0,0 +1,18 @@
+const width = 400;
+const height = 400;
+const fr = 1;
+const lines = 10;
+const maxSize = 10;
+function setup() {
+	createCanvas(width, height);
+	frameRate(fr);
+}
+
+function draw() {
+	// background(255, 204, 0);
+	background(55);
+	for (let i = 0; i < lines; i++) {
+		strokeWeight(random(maxSize));
+		line(random(width), random(height), random(width), random(height));
+	}
+}
