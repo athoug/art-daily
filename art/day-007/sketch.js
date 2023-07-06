99bf5e6a6c55556a578e2bd58f5c08000236e3e0
Athoug <athoug.s@gmail.com>
2023-07-06 07:26:20 +0300

    day 7

--- art/day-007/sketch.js
+++ art/day-007/sketch.js
@@ -5,4 +5,10 @@ function setup() {
 	createCanvas(width, height);
 }
 
-function draw() {}
+function draw() {
+	stroke(random(200, 255), random(100, 155), random(100, 155));
+
+	let x = random(width);
+
+	line(x, 0, x, height);
+}
