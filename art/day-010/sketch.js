8dc4eb40524e501f29e19769ba0c1b4f75363513
Athoug <athoug.s@gmail.com>
2023-07-09 08:05:00 +0300

    add new sketch

--- art/day-010/sketch.js
+++ art/day-010/sketch.js
@@ -1,8 +1,73 @@
 const width = 400;
 const height = 400;
 
+let quad1 = false;
+let quad2 = false;
+let quad3 = false;
+let quad4 = false;
+
 function setup() {
 	createCanvas(width, height);
+	noFill();
+	stroke(255);
+	strokeWeight(4);
 }
 
-function draw() {}
+function draw() {
+	background(51);
+	quadCheck();
+
+	if (quad1) {
+		fill(255);
+	} else {
+		noFill();
+	}
+	ellipse(100, 100, 100, 100);
+
+	if (quad2) {
+		fill(255);
+	} else {
+		noFill();
+	}
+	ellipse(300, 100, 100, 100);
+
+	if (quad3) {
+		fill(255);
+	} else {
+		noFill();
+	}
+	ellipse(100, 300, 100, 100);
+
+	if (quad4) {
+		fill(255);
+	} else {
+		noFill();
+	}
+	ellipse(300, 300, 100, 100);
+}
+
+function quadCheck() {
+	if (mouseX < 200 && mouseY < 200) {
+		quad1 = true;
+	} else {
+		quad1 = false;
+	}
+
+	if (mouseX > 200 && mouseY < 200) {
+		quad2 = true;
+	} else {
+		quad2 = false;
+	}
+
+	if (mouseX < 200 && mouseY > 200) {
+		quad3 = true;
+	} else {
+		quad3 = false;
+	}
+
+	if (mouseX > 200 && mouseY > 200) {
+		quad4 = true;
+	} else {
+		quad4 = false;
+	}
+}
