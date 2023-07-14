a5c5bb1f6c99dc77d656bf6b113abf85b71ac3b5
Athoug <athoug.s@gmail.com>
2023-07-14 09:22:34 +0300

    version 1

--- art/day-015/v1.js
+++ art/day-015/v1.js
@@ -1,8 +1,30 @@
 const width = 400;
 const height = 400;
+let x = 0;
+
+const halfHight = height / 2;
 
 function setup() {
 	createCanvas(width, height);
+	background(255);
+	stroke(31);
 }
 
-function draw() {}
+function draw() {
+	line(x, halfHight, x, halfHight - 100);
+	x += 1;
+
+	if (x > width) {
+		x = 0;
+	}
+
+	// deciding to change the color
+	if (random(100) > 70) {
+		// deciding which color to change it to
+		if (random(100) > 50) {
+			stroke(255);
+		} else {
+			stroke(31);
+		}
+	}
+}
