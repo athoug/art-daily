c44fd362a6bbffa208cce73eb10614bc1106858c
Athoug <athoug.s@gmail.com>
2023-07-14 10:05:43 +0300

    version 2

--- art/day-015/sketch.js
+++ art/day-015/sketch.js
@@ -1,8 +1,36 @@
 const width = 400;
 const height = 400;
+let x = 0;
+
+const halfHight = height / 2;
 
 function setup() {
 	createCanvas(width, height);
+	background(255);
+	stroke(31);
 }
 
-function draw() {}
+function changeColors() {
+	stroke('#FABC2A');
+	line(x, halfHight, x, halfHight - 100);
+	// deciding which color to change it to
+	if (random(100) > 50) {
+		stroke(255);
+	} else {
+		stroke(31);
+	}
+}
+
+function draw() {
+	line(x, halfHight, x, halfHight - 100);
+
+	// deciding to change the color
+	if (random(100) > 70) {
+		changeColors();
+	}
+
+	x += 1;
+	if (x > width) {
+		x = 0;
+	}
+}
