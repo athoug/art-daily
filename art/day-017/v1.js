f06575d076de48924441227d9a7849f457c2a0f1
Athoug <athoug.s@gmail.com>
2023-07-16 07:54:00 +0300

    extra

--- art/day-017/v1.js
+++ art/day-017/v1.js
@@ -1,8 +1,27 @@
 const width = 400;
 const height = 400;
 
+const centerX = width / 2;
+const centerY = height / 2;
+
+let size = 100;
+
 function setup() {
 	createCanvas(width, height);
+	noStroke();
+
+	textAlign(CENTER, CENTER);
 }
 
-function draw() {}
+function draw() {
+	background(31);
+
+	circle(centerX, centerY, size);
+	size += 1;
+
+	text('click for reset', centerX, centerY);
+}
+
+function mousePressed() {
+	size = 0;
+}
