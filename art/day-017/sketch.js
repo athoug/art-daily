73f29884b8f32fd7bfe21eb284b022abf6970611
Athoug <athoug.s@gmail.com>
2023-07-16 07:45:33 +0300

    starry night

--- art/day-017/sketch.js
+++ art/day-017/sketch.js
@@ -1,8 +1,18 @@
 const width = 400;
 const height = 400;
+let size = 8;
 
 function setup() {
 	createCanvas(width, height);
+	background('#273173');
+	noStroke();
 }
 
-function draw() {}
+function draw() {
+	fill('rgba(39, 49, 115, 0.1)');
+	rect(0, 0, width, height);
+
+	size = random(10);
+	fill('#FED315');
+	circle(random(width), random(height), size);
+}
