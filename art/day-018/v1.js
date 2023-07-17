6a8d1dcda38d43734db8528eaeb7a573a6a82048
Athoug <athoug.s@gmail.com>
2023-07-17 07:00:25 +0300

    version 1

--- art/day-018/v1.js
+++ art/day-018/v1.js
@@ -1,8 +1,18 @@
 const width = 400;
 const height = 400;
+colorMode(HSB);
 
 function setup() {
 	createCanvas(width, height);
-}
+	background(random(255), random(255), random(255));
+	noFill();
+	stroke(255, 100);
 
-function draw() {}
+	const x = random(width);
+	const y = random(height);
+	let i = 0;
+	while (i < 100) {
+		ellipse(x + i * 1.5, y + i * 1.5, 100 + i, 100 - i);
+		i += 1;
+	}
+}
