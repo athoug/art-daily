16ef7be5e36f7096ab139e8cdb7b360e5450724f
Athoug <athoug.s@gmail.com>
2023-07-18 07:14:19 +0300

    random circles

--- extra/random-circles/sketch.js
+++ extra/random-circles/sketch.js
@@ -1,8 +1,23 @@
-const width = 400;
-const height = 400;
+let r, g, b;
+let x, y;
+let size;
 
 function setup() {
-	createCanvas(width, height);
+	createCanvas(windowWidth, windowHeight);
+	noStroke();
+	background(255);
 }
 
-function draw() {}
+function draw() {
+	r = random(100, 255);
+	g = random(100, 255);
+	b = random(100, 255);
+
+	x = random(width);
+	y = random(height);
+
+	size = random(50);
+
+	fill(r, g, b, 100);
+	circle(x, y, size);
+}
