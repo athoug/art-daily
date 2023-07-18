cd2cd7d5fee3e02236913c6b84d229e12ebbc8e4
Athoug <athoug.s@gmail.com>
2023-07-18 06:38:44 +0300

    version 1

--- art/day-019/v1.js
+++ art/day-019/v1.js
@@ -1,8 +1,20 @@
-const width = 400;
 const height = 400;
+const width = 400;
+
+colorMode(HSB);
 
 function setup() {
 	createCanvas(width, height);
-}
+	background(random(255), random(255), random(255));
+	noFill();
+	stroke(255, 100);
 
-function draw() {}
+	const x = random(width);
+	const y = random(height);
+	let i = 0;
+
+	while (i < 80) {
+		ellipse(width / 2, height / 2, 100 + i * 3, 100 - i * 3);
+		i += 3;
+	}
+}
