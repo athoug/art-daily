7b061ca9117aa45b46de8ebbb0826e7205025fa4
Athoug <athoug.s@gmail.com>
2023-07-19 08:13:34 +0300

    v1

--- art/day-020/v1.js
+++ art/day-020/v1.js
@@ -1,8 +1,14 @@
-const width = 400;
-const height = 400;
+const width = 600;
+const height = 600;
 
 function setup() {
 	createCanvas(width, height);
-}
+	background('#514663');
+	fill('#4D5382');
+	textSize(200);
+	text('disco', 12, 300);
 
-function draw() {}
+	fill('#658E9C');
+	textSize(100);
+	text('astronaut', 20, 400);
+}
