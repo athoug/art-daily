3237925e9bb1830c2db2455502d3fed27f622fdc
Athoug <athoug.s@gmail.com>
2023-07-19 08:18:45 +0300

    v2

--- art/day-020/sketch.js
+++ art/day-020/sketch.js
@@ -1,8 +1,18 @@
-const width = 400;
-const height = 400;
+const width = 600;
+const height = 600;
 
 function setup() {
 	createCanvas(width, height);
+	background('#4D5382');
 }
 
-function draw() {}
+function draw() {
+	fill(77, 83, 130, 10);
+	rect(0, 0, width, height);
+}
+
+function keyPressed() {
+	fill('#658E9C');
+	textSize(random(20, 200));
+	text(key, random(20, 500), random(50, 550));
+}
