c1eb848c57de3d680042946253ad80b0863e87fc
Athoug <athoug.s@gmail.com>
2023-07-20 07:04:35 +0300

    map

--- extra/map-color/sketch.js
+++ extra/map-color/sketch.js
@@ -1,16 +1,22 @@
 const width = 400;
 const height = 400;
 
-let col = 0;
+let r = 0;
+let b = 255;
 
 function setup() {
 	createCanvas(width, height);
 }
 
 function draw() {
-	col = map(mouseX, 0, width, 0, 255);
+	r = map(mouseX, 0, width, 0, 255);
+	b = map(mouseY, 0, width, 0, 255);
+	background(r, 0, b);
 
-	background(col);
+	stroke(255, 0, 0);
+	line(0, mouseY, width, mouseY);
+	line(mouseX, 0, mouseX, height);
 	fill(100, 0, 200);
-	ellipse(mouseX, 200, 24, 24);
+	noStroke();
+	ellipse(mouseX, mouseY, 24, 24);
 }
