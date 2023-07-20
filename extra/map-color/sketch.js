073a3801154d703b56173598ed6f490dc5de4a71
Athoug <athoug.s@gmail.com>
2023-07-20 06:18:26 +0300

    map color updates

--- extra/map-color/sketch.js
+++ extra/map-color/sketch.js
@@ -1,8 +1,16 @@
 const width = 400;
 const height = 400;
 
+let col = 0;
+
 function setup() {
 	createCanvas(width, height);
 }
 
-function draw() {}
+function draw() {
+	col = map(mouseX, 0, width, 0, 255);
+
+	background(col);
+	fill(100, 0, 200);
+	ellipse(mouseX, 200, 24, 24);
+}
