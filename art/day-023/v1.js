433dcea9364f779099548eea0f394d24464dccbb
Athoug <athoug.s@gmail.com>
2023-07-22 09:00:45 +0300

    version 1

--- art/day-023/v1.js
+++ art/day-023/v1.js
@@ -1,8 +1,36 @@
 const width = 400;
 const height = 400;
 
+const halfW = width / 2;
+const halfH = height / 2;
+
+let r = 0;
+let circle_size = 0;
+let position = 50;
+let x = position;
+
 function setup() {
 	createCanvas(width, height);
+	background(31);
+	smooth();
+	noStroke();
 }
 
-function draw() {}
+function draw() {
+	translate(halfW, halfH);
+	rotate(r);
+	fill(255);
+
+	circle_size = random(5, 15);
+
+	ellipse(x, 20, circle_size);
+
+	r += 0.2;
+	x = position + r;
+
+	if (x > halfW) {
+		x = position;
+		r = 0;
+		background(31);
+	}
+}
