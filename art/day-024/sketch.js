132eef01d05f8c86fe7d5b60437eff8ce0b79027
Athoug <athoug.s@gmail.com>
2023-07-23 08:05:28 +0300

    v1

--- art/day-024/sketch.js
+++ art/day-024/sketch.js
@@ -1,8 +1,36 @@
 const width = 400;
 const height = 400;
 
+const halfW = width / 2;
+const halfH = height / 2;
+
+let red;
+let green;
+let blue;
+
+let r = 0;
+
 function setup() {
 	createCanvas(width, height);
+	smooth();
+	noStroke();
+	rectMode(CENTER);
+
+	red = random(0, 100);
+	green = random(0, 100);
+	blue = random(0, 100);
+
+	background(red, green, blue);
+
+	console.log('hi');
 }
 
-function draw() {}
+function draw() {
+	fill(red, green, blue, 10);
+	rect(halfW, halfH, width, height);
+	translate(mouseX, mouseY);
+	rotate(r);
+	fill(255);
+	rect(0, 0, 100, 100);
+	r += 0.05;
+}
