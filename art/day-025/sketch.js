c7c7ff5cbced19e9f6cea896bd6266bd4a3ee3a9
Athoug <athoug.s@gmail.com>
2023-07-24 08:20:46 +0300

    done

--- art/day-025/sketch.js
+++ art/day-025/sketch.js
@@ -1,8 +1,38 @@
 const width = 400;
 const height = 400;
 
+let r = 0;
+
 function setup() {
 	createCanvas(width, height);
+	smooth();
+	noStroke();
+	fill(31);
+	rectMode(CENTER);
 }
 
-function draw() {}
+function draw() {
+	background(255);
+
+	translate(100, 100);
+	rotate(r);
+	rect(0, 0, 80, 80);
+	resetMatrix();
+
+	translate(300, 100);
+	rotate(r);
+	rect(0, 0, 80, 80);
+	resetMatrix();
+
+	translate(100, 300);
+	rotate(r);
+	rect(0, 0, 80, 80);
+	resetMatrix();
+
+	translate(300, 300);
+	rotate(r);
+	rect(0, 0, 80, 80);
+	resetMatrix();
+
+	r += 0.02;
+}
