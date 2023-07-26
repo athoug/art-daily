c2e72be9b59df6d4a85726f373918a7953af6ece
Athoug <athoug.s@gmail.com>
2023-07-26 17:32:25 +0300

    okay this is cool

--- art/day-027/v1.js
+++ art/day-027/v1.js
@@ -2,48 +2,31 @@ const width = 400;
 const height = 400;
 
 let r = 0;
-
-let red;
-let green;
-let blue;
-
-let count = 0;
+let w = 25;
+let h = 100;
 
 function setup() {
 	createCanvas(width, height);
 	smooth();
 	noStroke();
 	rectMode(CENTER);
-	red = random(255);
-	green = random(255);
-	blue = random(255);
 }
 
 function draw() {
-	background(255);
+	background(200, 0, 100);
 
-	draw_rectangle(100, 100, 80);
-	draw_rectangle(300, 100, 40);
-	draw_rectangle(100, 300, 100);
-	draw_rectangle(300, 300, 20);
-	draw_rectangle(200, 200, 150);
-
+	draw_rectangle(120, 200, w, h);
+	draw_rectangle(170, 200, w, h);
+	draw_rectangle(220, 200, w, h);
+	draw_rectangle(270, 200, w, h);
 	r += 0.03;
 }
 
-function draw_rectangle(x, y, size) {
+function draw_rectangle(x, y, w, h) {
 	translate(x, y);
 	rotate(r);
 
-	if (random(100) > 95) {
-		if (random(100) > 95) {
-			red = random(255);
-			green = random(255);
-			blue = random(255);
-		}
-	}
-
-	fill(red, green, blue);
-	rect(0, 0, size);
+	fill(255);
+	rect(0, 0, w, h, 25);
 	resetMatrix();
 }
