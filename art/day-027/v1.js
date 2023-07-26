d7b49f795c15430124dca51c3843089caf5e4e21
Athoug <athoug.s@gmail.com>
2023-07-26 17:13:18 +0300

    add box shadow to template

--- art/day-027/v1.js
+++ art/day-027/v1.js
@@ -1,8 +1,19 @@
 const width = 400;
 const height = 400;
 
+let r = 0;
+
+let red;
+let green;
+let blue;
+
 function setup() {
 	createCanvas(width, height);
+	smooth();
+	noStroke();
+	rectMode(CENTER);
 }
 
-function draw() {}
+function draw() {
+	background(255);
+}
