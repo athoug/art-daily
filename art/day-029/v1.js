f7c537ebe9155c47fb11e659a034100331619881
Athoug <athoug.s@gmail.com>
2023-07-28 10:23:52 +0300

    version 1

--- art/day-029/v1.js
+++ art/day-029/v1.js
@@ -1,8 +1,22 @@
 const width = 400;
 const height = 400;
 
+let x = 0;
 function setup() {
 	createCanvas(width, height);
+	background(255);
+	noStroke();
+
+	while (x < width) {
+		let y = 0;
+		while (y < height) {
+			fill(random(10, 250));
+			rect(x, y, 38, 38);
+			y += 40;
+		}
+
+		x += 40;
+	}
 }
 
 function draw() {}
