41b9be3ae123d3560fe4d7c83050218945a4bea1
Athoug <athoug.s@gmail.com>
2023-07-28 10:39:58 +0300

    v2

--- art/day-029/sketch.js
+++ art/day-029/sketch.js
@@ -1,8 +1,33 @@
 const width = 400;
 const height = 400;
 
+let x = 0;
 function setup() {
 	createCanvas(width, height);
+	noStroke();
+
+	// frameRate(3);
+	background(10);
 }
 
-function draw() {}
+function draw() {
+	while (x < width) {
+		let y = 0;
+		while (y < height) {
+			if (random(100) > 96) {
+				fill(100, 10, 10);
+			} else {
+				fill(random(10, 250));
+			}
+			ellipse(x + 10, y + 10, 20, 20);
+
+			y += 20;
+		}
+		x += 20;
+	}
+	console.log(frameRate());
+	if (frameRate() >= 70) {
+		background(10);
+		x = 0;
+	}
+}
