4319f934300a54896ed368f8b516488c0aa5e4c9
Athoug <athoug.s@gmail.com>
2023-07-29 09:02:05 +0300

    version 1

--- art/day-030/v1.js
+++ art/day-030/v1.js
@@ -1,8 +1,49 @@
 const width = 400;
 const height = 400;
+let canvasRef;
+
+let r = 0;
+let s = 18;
+let frameSaved = 0;
+let margins = 50;
 
 function setup() {
-	createCanvas(width, height);
+	canvasRef = createCanvas(width, height);
+	smooth();
+	noStroke();
 }
 
-function draw() {}
+function draw() {
+	background(100, 0, 200);
+
+	let x = 0;
+	while (x < 10) {
+		let y = 0;
+		while (y < 10) {
+			draw_rotating_object(
+				margins * 1.25 + x * 30,
+				margins * 1.25 + y * 30,
+				s,
+				r + y + x
+			);
+			y += 1;
+		}
+		x += 1;
+	}
+
+	r += 0.02;
+}
+
+function draw_rotating_object(x, y, s, sp) {
+	translate(x, y);
+	rotate(sp);
+	rect(0, 0, s);
+	resetMatrix();
+}
+
+function keyPressed() {
+	if (key == 's') {
+		frameSaved += 1;
+		saveCanvas(canvasRef, `wave-${frameSaved}`, 'png');
+	}
+}
