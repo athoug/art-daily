d9a3d6bf9b2072f1142862c7e45568dcb33626e1
Athoug <athoug.s@gmail.com>
2023-07-29 09:21:05 +0300

    this is bloody cool

---
+++ art/day-030/v3.js
@@ -0,0 +1,48 @@
+const width = 400;
+const height = 400;
+
+let r = 0;
+const margin = 60;
+const spaceBetweenObjects = 30;
+let frameSaved = 0;
+let canvasRef;
+
+function setup() {
+	canvasRef = createCanvas(width, height);
+	smooth();
+	ellipseMode(CORNER);
+}
+
+function draw() {
+	background('#4D42AB');
+	let x = 0;
+	while (x < 10) {
+		let y = 0;
+		while (y < 10) {
+			fill(255 - y);
+			drr(
+				margin + x * spaceBetweenObjects,
+				margin + y * spaceBetweenObjects,
+				15,
+				r + sin(y) + cos(x)
+			);
+			y += 0.3;
+		}
+		x += 1;
+		r += 0.01;
+	}
+}
+
+function drr(x, y, s, sp) {
+	translate(x, y);
+	rotate(sp);
+	ellipse(0, 0, s);
+	resetMatrix();
+}
+
+function keyPressed() {
+	if (key == 's') {
+		frameSaved += 1;
+		saveCanvas(canvasRef, `worm-${frameSaved}`, 'png');
+	}
+}
