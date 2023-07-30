d1c5c590f7a0aa423dba7640075d28dc493f4c79
Athoug <athoug.s@gmail.com>
2023-07-30 06:43:58 +0300

    noise in loops

--- art/day-031/sketch.js
+++ art/day-031/sketch.js
@@ -1,8 +1,17 @@
 const width = 400;
 const height = 400;
 
+let noiseScale = 0.02;
+
 function setup() {
 	createCanvas(width, height);
 }
 
-function draw() {}
+function draw() {
+	background(250);
+	for (let x = 0; x < width; x++) {
+		const noiseVal = noise(mouseX + x * noiseScale, mouseY * noiseScale);
+		stroke(noiseVal * 100);
+		line(x, mouseY + noiseVal * 80, x, height);
+	}
+}
