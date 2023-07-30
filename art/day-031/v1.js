82867347810a5ec01d716b6381fc6640af3c157d
Athoug <athoug.s@gmail.com>
2023-07-30 06:35:31 +0300

    noise v1

--- art/day-031/v1.js
+++ art/day-031/v1.js
@@ -1,8 +1,27 @@
 const width = 400;
 const height = 400;
 
+let xOff = 0;
+let yOff = 0;
+
 function setup() {
 	createCanvas(width, height);
+	stroke(21);
+	strokeWeight(3);
+	noFill();
 }
 
-function draw() {}
+function draw() {
+	background(255);
+
+	let x = noise(xOff) * width;
+	line(x, 0, x, height);
+
+	let y = noise(yOff) * height;
+	line(0, y, width, y);
+
+	ellipse(x, y, 20);
+
+	xOff += 0.01;
+	yOff += 0.02;
+}
