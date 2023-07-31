53f4003971a29dd810f1a866678f5dc27df18c07
Athoug <athoug.s@gmail.com>
2023-07-31 06:19:45 +0300

    starter code

--- art/day-032/v1.js
+++ art/day-032/v1.js
@@ -1,8 +1,23 @@
 const width = 400;
 const height = 400;
 
+let seed = 0;
+
 function setup() {
 	createCanvas(width, height);
+	background(255);
+	smooth();
+	noStroke();
+	fill(31);
+	rectMode(CENTER);
 }
 
-function draw() {}
+function draw() {
+	background(255);
+
+	translate(width * noise(seed + 10), height * noise(seed + 20));
+	rotate(10 * noise(seed + 42));
+	rect(0, 0, 200 * noise(seed + 20), 200 * noise(seed + 40));
+
+	seed += 0.02;
+}
