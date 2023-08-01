3077e9fb21bf191bc293f2f84b4b327aa99097eb
Athoug <athoug.s@gmail.com>
2023-08-01 06:56:28 +0300

    exercise

--- art/day-033/v1.js
+++ art/day-033/v1.js
@@ -1,8 +1,23 @@
 const width = 400;
 const height = 400;
 
+let time = 0;
+const offset = height / 2;
+
 function setup() {
 	createCanvas(width, height);
+	stroke('#2f4858');
 }
 
-function draw() {}
+function draw() {
+	background('#86BBD8');
+
+	let x = 0;
+	while (x < width) {
+		line(x, offset + 50 * noise(x / 100, time), x, height);
+
+		x += 1;
+	}
+
+	time += 0.02;
+}
