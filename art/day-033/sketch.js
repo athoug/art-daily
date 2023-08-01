04332dc8ead2f1a51d228f07dd69cd8a2b69c6e7
Athoug <athoug.s@gmail.com>
2023-08-01 06:51:12 +0300

    final version

--- art/day-033/sketch.js
+++ art/day-033/sketch.js
@@ -1,8 +1,29 @@
 const width = 400;
 const height = 400;
 
+let time = 0;
+let strokeColor = 0;
+
 function setup() {
 	createCanvas(width, height);
 }
 
-function draw() {}
+function draw() {
+	let x = 0;
+
+	if (random(100) > 95) {
+		if (random(100) > 50) {
+			strokeColor = 0;
+		} else {
+			strokeColor = 255;
+		}
+	}
+	while (x < width) {
+		stroke(strokeColor);
+		point(x, height * noise(x / 100, time));
+
+		x += 1;
+	}
+
+	time += 0.02;
+}
