0bd8ef93f3de4da53415aab638e97ead1fe857b6
Athoug <athoug.s@gmail.com>
2023-08-02 20:03:14 +0300

    version 1

--- art/day-034/v1.js
+++ art/day-034/v1.js
@@ -1,8 +1,25 @@
 const width = 400;
 const height = 400;
+let z = 0;
 
 function setup() {
 	createCanvas(width, height);
+	noStroke();
+	colorMode(HSB);
 }
 
-function draw() {}
+function draw() {
+	let x = 0;
+	while (x < width) {
+		let y = 0;
+		while (y < height) {
+			let c = 255 * noise(x / 500, y / 500, z);
+			fill(c, 255, 255);
+			ellipse(x, y, 120, 120);
+			y += 40;
+		}
+		x += 40;
+	}
+
+	z += 0.02;
+}
