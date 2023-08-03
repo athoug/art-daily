7a7334331596c1602607241eb73e63fb21b751bd
Athoug <athoug.s@gmail.com>
2023-08-03 08:04:50 +0300

    playing around with sin

--- art/day-035/sketch.js
+++ art/day-035/sketch.js
@@ -1,8 +1,31 @@
 const width = 400;
 const height = 400;
 
+const halfH = height / 2;
+const halfW = width / 4;
+
 function setup() {
 	createCanvas(width, height);
 }
 
-function draw() {}
+let i = 0;
+
+function draw() {
+	let a = 0.0;
+	let inc = TWO_PI / 25;
+
+	// for (let i = 0; i < width; i++) {
+	// 	line(i * 4, halfH, i * 4, halfH + sin(a) * 40);
+	// 	a += inc;
+	// }
+
+	strokeWeight(5 * noise(i / 10));
+	line(i * 4, halfH, i * 4, halfH + sin(i) * random(100));
+	a += inc;
+	i += 1;
+
+	if (i > halfW) {
+		background(255);
+		i = 0;
+	}
+}
