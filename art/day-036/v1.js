f8b2fd4a718d2d3becf24e3b0a9ad6296ebe4578
Athoug <athoug.s@gmail.com>
2023-08-04 10:37:41 +0300

    another test with noise

--- art/day-036/v1.js
+++ art/day-036/v1.js
@@ -1,23 +1,33 @@
 const width = 400;
 const height = 400;
 
-let noiseX = 0;
-let noiseY = 0;
+let n = 0;
 
 function setup() {
 	createCanvas(width, height);
-	background('#E3B23C');
-	noStroke();
-	smooth();
+	background(255);
+	stroke(0);
+	colorMode(HSB);
 }
 
 function draw() {
-	background('#E3B23C');
+	background(255);
+	let x = 0;
+	let s = 0;
+	while (x < width) {
+		let i = 0;
+		while (i < width / 2) {
+			stroke(360 * noise(s), 255, 255);
+			strokeWeight(5 * noise(s));
+			let y = height * noise(s) * noise(n);
+			line(x, 0, x, 50 + y * sin(n));
 
-	let x = map(sin(noiseX), -1, 1, 0, width);
-	let y = map(sin(noiseY), -1, 1, 0, height);
-	ellipse(x, y, 30, 30);
+			s += 0.03;
+			i += 20;
+		}
 
-	noiseX += 0.02;
-	noiseY += 0.05;
+		x += 20;
+	}
+
+	n += 0.05;
 }
