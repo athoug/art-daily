92ebfb88d62ebf98a2ebf4933aed053106c1a08e
Athoug <athoug.s@gmail.com>
2023-08-05 08:47:30 +0300

    version 1

--- art/day-037/v1.js
+++ art/day-037/v1.js
@@ -1,8 +1,23 @@
 const width = 400;
 const height = 400;
 
+let x = 0;
+let a = 0;
+
 function setup() {
 	createCanvas(width, height);
+	background(51);
 }
 
-function draw() {}
+function draw() {
+	stroke(255);
+	strokeWeight(3);
+	while (x < width) {
+		let y = map(sin(a) * sin(a * 1.7) * sin(a * 1.8), -1, 1, 100, 250);
+
+		point(x, y);
+
+		x += 1;
+		a += 0.03;
+	}
+}
