214da6e01869621cc0b678b0eb50073f273b304b
Athoug <athoug.s@gmail.com>
2023-08-08 05:51:32 +0300

    I don't know

--- art/day-040/sketch.js
+++ art/day-040/sketch.js
@@ -1,8 +1,48 @@
 const width = 400;
 const height = 400;
 
+let change = 0;
+let x = -5;
+let y = -10;
+
 function setup() {
 	createCanvas(width, height);
+	noFill();
+	smooth();
+	background(250);
+	rectMode(CENTER);
 }
 
-function draw() {}
+function draw() {
+	if (random(100) > 50) {
+		stroke('#b1ff0a');
+	} else {
+		stroke('#315500');
+	}
+
+	let r = random(200);
+
+	ellipse(200, 200, r);
+
+	copy(
+		0,
+		0,
+		width,
+		height,
+		x,
+		y,
+		width - 10 * noise(change),
+		height - 10 * noise(change)
+	);
+
+	x += 0.02;
+	y += 0.05;
+	change += 0.03;
+
+	if (x > 10) {
+		x = -5;
+	}
+	if (y > 10) {
+		y = -10;
+	}
+}
