4a0e127a0eae3f96bfec27d148ceb4fb847ad69d
Athoug <athoug.s@gmail.com>
2023-08-09 06:34:19 +0300

    sketch

--- art/day-041/sketch.js
+++ art/day-041/sketch.js
@@ -1,8 +1,29 @@
-const width = 400;
-const height = 400;
+const width = 600;
+const height = 600;
+
+let photo;
+let x = 0;
+let y = 0;
+let a = 0;
+function preload() {
+	photo = loadImage('./assets/eye.gif');
+}
 
 function setup() {
 	createCanvas(width, height);
 }
 
-function draw() {}
+function draw() {
+	background(0);
+	image(photo, x * noise(a * 1.7), y * noise(a * 0.7));
+
+	if (x < 100) {
+		x += 1;
+	}
+
+	if (y < 100) {
+		y += 1;
+	}
+
+	a += 0.03;
+}
