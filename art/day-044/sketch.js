34e74b3fedf35b2ee9625b9268579fafb066df75
Athoug <athoug.s@gmail.com>
2023-08-12 07:14:56 +0300

    code version

--- art/day-044/sketch.js
+++ art/day-044/sketch.js
@@ -1,8 +1,24 @@
 const width = 400;
 const height = 400;
 
+let photo;
+let a = 0;
+
+function preload() {
+	photo = loadImage('./assets/image.gif');
+}
+
 function setup() {
 	createCanvas(width, height);
 }
 
-function draw() {}
+function draw() {
+	let x = map(noise(a * 1.2), 0, 1, 100, 400);
+	let y = map(noise(a), 0, 1, 100, 300);
+	let w = map(noise(a * 1.7), 0, 1, 100, 400);
+	let h = map(noise(a * 1.4), 0, 1, 100, 400);
+
+	copy(photo, x, y, w, h, 0, y, width, y);
+
+	a += 0.01;
+}
