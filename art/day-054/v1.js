147b7ea45323424f673e9f57fcb31498317f9f89
Athoug <athoug.s@gmail.com>
2023-08-22 07:56:46 +0300

    waves falling

--- art/day-054/v1.js
+++ art/day-054/v1.js
@@ -1,8 +1,30 @@
 const width = 400;
 const height = 400;
 
+const x = [];
+const y = [];
+let a = 0;
+
 function setup() {
 	createCanvas(width, height);
 }
 
-function draw() {}
+function draw() {
+	let i = 0;
+
+	while (i < x.length) {
+		point(x[i], y[i]);
+
+		y[i] += noise(a * 1.7);
+		x[i] += random(-2, 2);
+
+		i += 1;
+	}
+
+	a += 0.01;
+}
+
+function mousePressed() {
+	x.push(mouseX);
+	y.push(mouseY);
+}
