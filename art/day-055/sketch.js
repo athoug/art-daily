3bc477040a774fe975f75a8774f96673d3b4092f
Athoug <athoug.s@gmail.com>
2023-08-23 08:08:46 +0300

    vesrions 1 and 2

--- art/day-055/sketch.js
+++ art/day-055/sketch.js
@@ -1,8 +1,60 @@
 const width = 400;
 const height = 400;
 
+let x = [];
+let y = [];
+let move_x = [];
+let move_y = [];
+
 function setup() {
 	createCanvas(width, height);
+	smooth();
 }
 
-function draw() {}
+function draw() {
+	fill(255, 10);
+	noStroke();
+	rect(0, 0, width, height);
+
+	noFill();
+	stroke('#ff5e5b');
+	let i = 0;
+	while (i < x.length) {
+		const sz = random(10, 40);
+		noFill();
+		ellipse(x[i] + random(-3, 3), y[i] + random(-3, 3), sz, sz);
+
+		x[i] += move_x[i];
+		y[i] += move_y[i];
+
+		if (x[i] > width) {
+			x[i] = width;
+			move_x[i] *= -1;
+		}
+
+		if (y[i] > height) {
+			y[i] = height;
+			move_y[i] *= -1;
+		}
+
+		if (x[i] < 0) {
+			x[i] = 0;
+			move_x[i] *= -1;
+		}
+
+		if (y[i] < 0) {
+			y[i] = 0;
+			move_y[i] *= -1;
+		}
+
+		i += 1;
+	}
+}
+
+function mousePressed() {
+	x.push(mouseX);
+	y.push(mouseY);
+
+	move_x.push(random(-2, 2));
+	move_y.push(random(-2, 2));
+}
--- art/day-055/v1.js
+++ art/day-055/v1.js
@@ -1,8 +1,32 @@
 const width = 400;
 const height = 400;
 
+const x = [];
+const y = [];
+const char = [];
+
 function setup() {
 	createCanvas(width, height);
 }
 
-function draw() {}
+function draw() {
+	background(255);
+
+	let i = 0;
+	while (i < x.length) {
+		text(char[i], x[i], y[i]);
+		x[i] += random(-2, 2);
+		y[i] += random(-2, 2);
+
+		i += 1;
+	}
+}
+
+function keyPressed() {
+	x.push(mouseX);
+	y.push(mouseY);
+	char.push(key);
+
+	console.log('clicked');
+	console.log(key);
+}
