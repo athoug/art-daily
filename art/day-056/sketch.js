be3c8819add12e8201af0e086bfc177efed1a445
Athoug <athoug.s@gmail.com>
2023-08-24 08:23:35 +0300

    first 3d drawing ... kind of

--- art/day-056/sketch.js
+++ art/day-056/sketch.js
@@ -1,8 +1,24 @@
 const width = 400;
 const height = 400;
 
+let a = 0;
+let r = 0;
+
 function setup() {
-	createCanvas(width, height);
+	createCanvas(width, height, WEBGL);
 }
 
-function draw() {}
+function draw() {
+	background(80);
+	translate(
+		mouseX - width / 2,
+		mouseY - height / 2,
+		map(noise(a), 0, 1, -400, 400)
+	);
+
+	rotateY(r);
+	box();
+
+	a += 0.01;
+	r += 0.05;
+}
