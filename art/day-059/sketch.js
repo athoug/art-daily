18a8ffae3bab9400e572b102106b755a21112491
Athoug <athoug.s@gmail.com>
2023-08-27 07:34:39 +0300

    seeds

--- art/day-059/sketch.js
+++ art/day-059/sketch.js
@@ -1,8 +1,28 @@
 const width = 400;
 const height = 400;
 
+let seed;
 function setup() {
 	createCanvas(width, height);
+	colorMode(HSB, 100);
+	noStroke();
+	frameRate(2);
+
+	seed = random(10000000);
+	randomSeed(seed);
+
+	console.log(seed);
 }
 
-function draw() {}
+function draw() {
+	let x = 0;
+
+	while (x < width) {
+		let wi = random(100);
+
+		fill(random(100), 80, 80);
+		rect(x, 0, wi, height);
+
+		x += wi;
+	}
+}
