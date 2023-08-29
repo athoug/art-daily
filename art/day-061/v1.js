e5361eea555f092ef5c1540e4aeb902c5e892574
Athoug <athoug.s@gmail.com>
2023-08-29 07:44:04 +0300

    version 1

--- art/day-061/v1.js
+++ art/day-061/v1.js
@@ -3,8 +3,24 @@ const height = 400;
 const halfW = width / 2;
 const halfH = height / 2;
 
+let a = 0;
+const s = 10;
+
 function setup() {
 	createCanvas(width, height);
+	smooth();
 }
 
-function draw() {}
+function draw() {
+	let r = random(150, 180);
+	let x = halfW + cos(a) * r;
+	let y = halfH + sin(a) * r;
+
+	ellipse(x, y, s, s);
+
+	a += 0.1;
+
+	if (a > TWO_PI) {
+		noLoop();
+	}
+}
