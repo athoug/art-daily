e4c2747b10f2b0ebd0718fc03a3361e0222c885d
Athoug <athoug.s@gmail.com>
2023-08-30 07:07:54 +0300

    so many circular motions

--- art/day-062/sketch.js
+++ art/day-062/sketch.js
@@ -3,8 +3,71 @@ const height = 400;
 const halfW = width / 2;
 const halfH = height / 2;
 
+let x = 10;
+let y = 10;
+let a = 0;
+let b = 0;
+
+let s = 20;
+
+let x2r = 65;
+let y2r = 75;
+
+let x3r = 120;
+let y3r = 110;
+
+let rounds = 0;
+let c = '#020887';
+
 function setup() {
 	createCanvas(width, height);
+	noStroke();
+	smooth();
+	background(255);
 }
 
-function draw() {}
+function draw() {
+	fill(c);
+
+	x = halfW;
+	y = halfH;
+
+	let x2 = sin(a) * x2r;
+	let y2 = cos(a) * y2r;
+
+	let x3 = sin(b) * x3r;
+	let y3 = cos(b) * y3r;
+
+	ellipse(x + x2 + x3, y + y2 + y3, s, s);
+
+	a += 0.1;
+	b += 0.01;
+
+	if (b > TWO_PI) {
+		console.log('full circle');
+		rounds += 1;
+		b = 0;
+
+		if (rounds === 1) {
+			c = '#334195';
+			s = 15;
+
+			x2r = 45;
+			y2r = 55;
+
+			x3r = 100;
+			y3r = 90;
+		}
+
+		if (rounds === 2) {
+			c = '#647AA3';
+			s = 10;
+
+			x2r = 25;
+			y2r = 35;
+
+			x3r = 80;
+			y3r = 70;
+		}
+	}
+}
--- art/day-062/v1.js
+++ art/day-062/v1.js
@@ -3,8 +3,113 @@ const height = 400;
 const halfW = width / 2;
 const halfH = height / 2;
 
-function setup() {
-	createCanvas(width, height);
+let x = 10;
+let y = 10;
+let a = 0;
+let b = 0;
+
+let s = 10;
+let moveType = 'circular';
+
+// normal js stuff
+controls = document.querySelectorAll('.control-btns');
+
+for (let i = 0; i < controls.length; i++) {
+	controls[i].addEventListener('click', (e) => {
+		e.preventDefault();
+		moveType = e.target.textContent;
+		console.log(moveType);
+	});
 }
 
-function draw() {}
+function setup() {
+	createCanvas(width, height);
+	noStroke();
+	smooth();
+	background(255);
+}
+
+function draw() {
+	fill('#334195');
+
+	if (moveType === 'forward') {
+		x += 1;
+		ellipse(x, y, s, s);
+	}
+
+	if (moveType === 'down') {
+		y += 1;
+		ellipse(x, y, s, s);
+	}
+
+	if (moveType === 'diagonal') {
+		x += 1;
+		y += 1;
+		ellipse(x, y, s, s);
+	}
+
+	if (moveType === 'nothing') {
+		x += 1;
+		x -= 1;
+		ellipse(x, y, s, s);
+	}
+
+	if (moveType === 'oscillating') {
+		x += 1;
+		y += 1;
+
+		let x2 = sin(a) * 25;
+		ellipse(x + x2, y, s, s);
+
+		a += 0.1;
+	}
+
+	if (moveType === 'circular') {
+		x += 1;
+		y += 1;
+
+		let x2 = sin(a) * 25;
+		let y2 = cos(a) * 35;
+		ellipse(x + x2, y + y2, s, s);
+
+		a += 0.1;
+	}
+
+	if (moveType === 'random circular') {
+		x += 1;
+		y += 1;
+
+		let x2 = sin(a) * 25;
+		let y2 = cos(a) * 35;
+
+		let x3 = random(-3, 3);
+		let y3 = random(-3, 3);
+		ellipse(x + x2 + x3, y + y2 + y3, s, s);
+
+		a += 0.1;
+	}
+
+	if (moveType === 'double circular') {
+		x = halfW;
+		y = halfH;
+
+		let x2 = sin(a) * 25;
+		let y2 = cos(a) * 35;
+
+		let x3 = sin(b) * 80;
+		let y3 = cos(b) * 70;
+
+		ellipse(x + x2 + x3, y + y2 + y3, s, s);
+
+		a += 0.1;
+		b += 0.01;
+	}
+
+	if (x > width) {
+		x = 0;
+	}
+
+	if (y > height) {
+		y = 0;
+	}
+}
