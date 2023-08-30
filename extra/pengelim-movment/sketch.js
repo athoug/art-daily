7c70ad2ee838ba5bb6425920082a90f958753130
Athoug <athoug.s@gmail.com>
2023-08-30 07:27:27 +0300

    pengelim

--- extra/pengelim-movment/sketch.js
+++ extra/pengelim-movment/sketch.js
@@ -5,6 +5,27 @@ const halfH = height / 2;
 
 function setup() {
 	createCanvas(width, height);
+	smooth();
 }
 
-function draw() {}
+function draw() {
+	background(0);
+
+	// plain circular motion
+	//float angle = frameCount * 0.05;
+
+	// change this to define which part of the circle we are at (up, down, left...)
+	let angleOffset = 1.6;
+	// change this to define how wide is the motion
+	let angleSpread = 1.2;
+
+	// polar coordinates
+	// instead of having the angle increase forever, sine makes
+	// it periodic.
+	let angle = angleOffset + angleSpread * sin(frameCount * 0.05);
+	let distance = 40;
+	let x = width / 2 + distance * cos(angle);
+	let y = height / 2 + distance * sin(angle);
+
+	ellipse(x, y, 5, 5);
+}
