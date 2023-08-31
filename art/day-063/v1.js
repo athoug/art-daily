e03cda14d1ba6cfd334678d7770b36c2ce9a941e
Athoug <athoug.s@gmail.com>
2023-08-31 09:16:52 +0300

    change directions v1

--- art/day-063/v1.js
+++ art/day-063/v1.js
@@ -3,8 +3,37 @@ const height = 400;
 const halfW = width / 2;
 const halfH = height / 2;
 
-function setup() {
-	createCanvas(width, height);
+let oldx;
+let oldy;
+let a;
+
+function reset() {
+	oldx = halfW;
+	oldy = halfH;
+	a = random(TWO_PI);
 }
 
-function draw() {}
+function setup() {
+	createCanvas(width, height);
+	background(255);
+	colorMode(HSB, 100);
+	smooth();
+	noFill();
+	reset();
+}
+
+function draw() {
+	let newx = oldx + cos(a) * 5;
+	let newy = oldy + sin(a) * 5;
+
+	line(oldx, oldy, newx, newy);
+
+	oldx = newx;
+	oldy = newy;
+
+	a += random(-0.4, 0.4);
+
+	if (oldx < 0 || oldy < 0 || oldx > width || oldy > height) {
+		reset();
+	}
+}
