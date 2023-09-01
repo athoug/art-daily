0e0696888ac995f1c0a86c983047e776cdb76afb
Athoug <athoug.s@gmail.com>
2023-09-01 11:18:43 +0300

    grid pixel

--- art/day-064/sketch.js
+++ art/day-064/sketch.js
@@ -3,8 +3,40 @@ const height = 400;
 const halfW = width / 2;
 const halfH = height / 2;
 
+const s = 40;
+let n = 0;
+let d;
+let r;
+let c = 0;
+
 function setup() {
 	createCanvas(width, height);
+	noStroke();
+	fill(pink);
+	frameRate(4);
+	colorMode(HSB, 100);
+	d = random(1, 11);
+	r = int(random(9));
 }
 
-function draw() {}
+function draw() {
+	background(100);
+
+	for (let x = 0; x < width / s; x++) {
+		for (let y = 0; y < height / s; y++) {
+			fill(noise(c) * 100, noise(x) * 100, noise(y) * 100);
+			if (n % d == r) {
+				rect(x * s, y * s, s, s);
+			}
+
+			if (n % d == r + 3) {
+				rect(x * s, y * s, s, s);
+			}
+			n += 1;
+
+			c += 0.01;
+		}
+	}
+	d = int(random(100));
+	r = int(random(9));
+}
