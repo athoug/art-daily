116e1a1df2827126acb391ce77090e80d7528325
Athoug <athoug.s@gmail.com>
2023-09-04 07:25:22 +0300

    Update v1.js

--- art/day-067/v1.js
+++ art/day-067/v1.js
@@ -3,8 +3,27 @@ const height = 400;
 const halfW = width / 2;
 const halfH = height / 2;
 
+const curveCount = 10;
+
 function setup() {
 	createCanvas(width, height);
+	smooth();
+	noFill();
+	frameRate(2);
 }
 
-function draw() {}
+function draw() {
+	background(255);
+	for (let i = 0; i < curveCount; i++) {
+		bezier(
+			width / 2,
+			height,
+			random(width),
+			random(height),
+			random(width),
+			random(height),
+			random(width),
+			random(height)
+		);
+	}
+}
