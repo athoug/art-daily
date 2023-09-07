d240b2129c6c670d7f9e0e59314fd0316e57429c
Athoug <athoug.s@gmail.com>
2023-09-07 07:50:16 +0300

    spherical rotation

--- art/day-070/v1.js
+++ art/day-070/v1.js
@@ -1,10 +1,37 @@
-const width = 400;
-const height = 400;
+const width = 500;
+const height = 500;
 const halfW = width / 2;
 const halfH = height / 2;
 
+const amount = 300;
+const x = [];
+const y = [];
+const z = [];
 function setup() {
-	createCanvas(width, height);
+	createCanvas(width, height, WEBGL);
+	smooth();
+	background(31);
+	noFill();
+	stroke(255);
+	strokeWeight(2);
+
+	for (let i = 0; i < amount; i++) {
+		const r = 150;
+		const a = random(TWO_PI);
+		const b = random(TWO_PI);
+
+		x[i] = int(r * sin(a) * cos(b));
+		y[i] = int(r * sin(a) * sin(b));
+		z[i] = int(r * cos(a));
+	}
 }
 
-function draw() {}
+function draw() {
+	background(31);
+	translate(0, 0);
+	rotateY(frameCount / 100);
+
+	for (let i = 0; i < amount; i++) {
+		point(x[i], y[i], z[i]);
+	}
+}
