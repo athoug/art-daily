d6f118ce3ef62259c2abae48096ee97b28656f58
Athoug <athoug.s@gmail.com>
2023-09-08 13:17:26 +0300

    Update sketch.js

--- art/day-071-extra-2/sketch.js
+++ art/day-071-extra-2/sketch.js
@@ -1,10 +1,44 @@
-const width = 400;
-const height = 400;
+const width = 500;
+const height = 500;
 const halfW = width / 2;
 const halfH = height / 2;
 
+const amount = 300;
+const x = [];
+const y = [];
+const z = [];
+
 function setup() {
-	createCanvas(width, height);
+	createCanvas(width, height, WEBGL);
+	smooth();
+	noFill();
+	stroke('#A9E5BB');
+
+	const r = 200;
+
+	for (let i = 0; i < amount; i++) {
+		const a = random(TWO_PI);
+		const b = random(TWO_PI);
+		x[i] = sin(a) * cos(b) * r;
+		y[i] = sin(a) * sin(b) * r;
+		z[i] = cos(a) * r;
+	}
 }
 
-function draw() {}
+function draw() {
+	background(31);
+
+	rotateY(frameCount / 100);
+	const t = frameCount / 100;
+
+	for (let i = 0; i < amount; i++) {
+		push();
+		translate(
+			x[i] * noise(2, i, t),
+			y[i] * noise(1, i, t),
+			z[i] * noise(1, i, t)
+		);
+		ellipse(0, 0, 20, 20);
+		pop();
+	}
+}
