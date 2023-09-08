a614cb4a169c95a7d5491187228b0f99ed69b547
Athoug <athoug.s@gmail.com>
2023-09-08 12:46:41 +0300

    Update v1.js

--- art/day-071-extra/v1.js
+++ art/day-071-extra/v1.js
@@ -1,10 +1,28 @@
-const width = 400;
-const height = 400;
+const width = 500;
+const height = 500;
 const halfW = width / 2;
 const halfH = height / 2;
 
+const amount = 300;
+
 function setup() {
-	createCanvas(width, height);
+	createCanvas(width, height, WEBGL);
+	smooth();
+	fill('#F3A712');
+	stroke('#D8950F');
 }
 
-function draw() {}
+function draw() {
+	background(31);
+
+	rotateY(frameCount / 100);
+
+	for (let i = 0; i < amount; i++) {
+		push();
+		rotateZ((TWO_PI * i) / amount);
+		translate(100, 0, 0);
+		rotateX(PI / 2);
+		ellipse(0, 0, 50, 50);
+		pop();
+	}
+}
