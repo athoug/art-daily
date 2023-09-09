16d203234b07dcfc56d6fd651721f247b7fb8788
Athoug <athoug.s@gmail.com>
2023-09-09 10:43:47 +0300

    Update v1.js

--- art/day-072/v1.js
+++ art/day-072/v1.js
@@ -1,10 +1,31 @@
-const width = 400;
-const height = 400;
+const width = 450;
+const height = 450;
 const halfW = width / 2;
 const halfH = height / 2;
 
+const pixels = [];
+
 function setup() {
 	createCanvas(width, height);
-}
 
-function draw() {}
+	for (let x = 0; x < width; x++) {
+		for (let y = 0; y < 150; y++) {
+			stroke(random(255));
+			point(x, y);
+		}
+	}
+
+	for (let x = 0; x < width; x++) {
+		for (let y = 150; y < 200; y++) {
+			stroke(random(217, 255), random(148, 200), random(100));
+			point(x, y);
+		}
+	}
+
+	for (let x = 0; x < width; x++) {
+		for (let y = 200; y < height; y++) {
+			stroke(random(255));
+			point(x, y);
+		}
+	}
+}
