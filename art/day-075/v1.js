0f1f60982b41e766233bbd0b9f8fbbaa854f66bf
Athoug <athoug.s@gmail.com>
2023-09-12 05:40:26 +0300

    version 1

--- art/day-075/v1.js
+++ art/day-075/v1.js
@@ -3,8 +3,29 @@ const height = 400;
 const halfW = width / 2;
 const halfH = height / 2;
 
+let bg;
+let d = 20;
+
+function preload() {
+	bg = loadImage('data/big.png');
+}
 function setup() {
 	createCanvas(width, height);
+	smooth();
+	noFill();
+	stroke(255);
+	strokeWeight(3);
 }
 
-function draw() {}
+function draw() {
+	background(bg);
+	ellipse(mouseX, mouseY, d, d);
+
+	if (mouseIsPressed) {
+		d++;
+	}
+}
+
+function mouseReleased() {
+	d = 20;
+}
