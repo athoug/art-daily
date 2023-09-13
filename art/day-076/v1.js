79bb2f39da43095cfe090821576055fac0fd9ef1
Athoug <athoug.s@gmail.com>
2023-09-13 06:41:05 +0300

    Update v1.js

--- art/day-076/v1.js
+++ art/day-076/v1.js
@@ -1,10 +1,35 @@
-const width = 400;
+const width = 500;
 const height = 400;
 const halfW = width / 2;
 const halfH = height / 2;
 
+let back_image;
+let d = 20;
+
+function preload() {
+	back_image = loadImage('./assets/screenshot.png');
+}
 function setup() {
 	createCanvas(width, height);
+	smooth();
+	noFill();
 }
 
-function draw() {}
+function draw() {
+	background(back_image);
+	ellipse(mouseX, mouseY, d, d);
+
+	if (mouseIsPressed) {
+		d++;
+	}
+}
+
+function mouseReleased() {
+	loadPixels();
+	back_image.loadPixels();
+	for (let i = 0; i < pixels.length; i++) {
+		back_image.pixels[i] = pixels[i];
+	}
+	back_image.updatePixels();
+	d = 20;
+}
