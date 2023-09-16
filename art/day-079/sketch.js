07596b563571ebec9b620529196e0df2812e6fec
Athoug <athoug.s@gmail.com>
2023-09-16 09:32:34 +0300

    v2

--- art/day-079/sketch.js
+++ art/day-079/sketch.js
@@ -1,10 +1,55 @@
 const width = 400;
-const height = 400;
+const height = 534;
 const halfW = width / 2;
 const halfH = height / 2;
 
+let img;
+let d;
+let imageSize;
+let a = 0;
+
+function preload() {
+	img = loadImage('assets/istanbul.jpg');
+}
 function setup() {
 	createCanvas(width, height);
+	colorMode(HSB);
+	image(img, 0, 0);
+
+	d = pixelDensity();
+	imageSize = 4 * (width * d) * (height * d);
+
+	// frameRate(2);
 }
 
-function draw() {}
+function draw() {
+	loadPixels();
+
+	for (let i = 0; i < imageSize - 3; i++) {
+		const h = pixels[i];
+		const s = pixels[i + 1];
+		const b = pixels[i + 2];
+		const a = pixels[i + 3];
+
+		pixels[i] = h + sin(i, a);
+		pixels[i + 1] = s + noise(i + 1, a);
+		pixels[i + 2] = b;
+		pixels[i + 3] = a;
+
+		// if (b > 200) {
+		// 	pixels[i] = 255;
+		// 	pixels[i + 1] = 255;
+		// 	pixels[i + 2] = 255;
+		// 	pixels[i + 3] = 255;
+		// } else {
+		// 	pixels[i] = 0;
+		// 	pixels[i + 1] = 0;
+		// 	pixels[i + 2] = 0;
+		// 	pixels[i + 3] = 255;
+		// }
+	}
+
+	updatePixels();
+
+	a += 0.02;
+}
--- art/day-079/v1.js
+++ art/day-079/v1.js
@@ -29,27 +29,10 @@ function draw() {
 		const b = pixels[i + 2];
 		const a = pixels[i + 3];
 
-		// console.log('r', pixels[i]);
-		// console.log('g', pixels[i + 1]);
-		// console.log('b', pixels[i + 2]);
-		// console.log('a', pixels[i + 3]);
-
 		pixels[i] = g;
 		pixels[i + 1] = r;
 		pixels[i + 2] = b;
 		pixels[i + 3] = a;
-
-		// if (b > 200) {
-		// 	pixels[i] = g;
-		// 	pixels[i + 1] =r;
-		// 	pixels[i + 2] = b;
-		// 	pixels[i + 3] = a;
-		// } else {
-		// 	pixels[i] = red(color(0));
-		// 	pixels[i + 1] = green(color(0));
-		// 	pixels[i + 2] = blue(color(0));
-		// 	pixels[i + 3] = alpha(color(0));
-		// }
 	}
 
 	updatePixels();
