50393de21f10c8377b9e1f404a78db3a1fe9ad75
Athoug <athoug.s@gmail.com>
2023-09-17 06:51:29 +0300

    Update v1.js

--- art/day-080/v1.js
+++ art/day-080/v1.js
@@ -3,8 +3,54 @@ const height = 400;
 const halfW = width / 2;
 const halfH = height / 2;
 
+let elipX;
+let elipY;
+let rectX;
+let rectY;
+let x;
+
+let ellipsSize = 100;
+let rectSize = 100;
+
 function setup() {
 	createCanvas(width, height);
+	background('#2E294E');
+	noStroke();
+	fill('#C5D86D');
+	frameRate(10);
 }
 
-function draw() {}
+function scene1() {
+	elipX = random(width);
+	elipY = random(height);
+
+	ellipse(elipX, elipY, ellipsSize, ellipsSize);
+}
+
+function scene2() {
+	rectX = random(width);
+	rectY = random(height);
+	rect(rectX, rectY, rectSize, rectSize);
+}
+
+function scene3() {
+	x = random(width);
+	stroke('#C5D86D');
+	noFill();
+	rect(x, 0, x, height);
+}
+
+function draw() {
+	background('#2E294E');
+	const m = millis();
+
+	if (m < 2000) {
+		scene1();
+	} else if (m < 12000) {
+		scene2();
+	} else if (m < 17000) {
+		scene3();
+	} else {
+		noLoop();
+	}
+}
