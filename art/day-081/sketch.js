d358899c880f939d68692aa9f3d0bfb93a207bc2
Athoug <athoug.s@gmail.com>
2023-09-18 07:36:33 +0300

    todays sketch

--- art/day-081/sketch.js
+++ art/day-081/sketch.js
@@ -3,8 +3,37 @@ const height = 400;
 const halfW = width / 2;
 const halfH = height / 2;
 
+let bgColor;
+let x = halfW;
+let y = halfH;
+let s = 100;
+
 function setup() {
 	createCanvas(width, height);
+	smooth();
+	colorMode(HSB);
+	fill(random(255), 100, 200);
+	bgColor = color(random(255), 150, 255);
 }
 
-function draw() {}
+function draw() {
+	background(bgColor);
+
+	if (dist(x, y, mouseX, mouseY) < s / 2) {
+		stroke(255);
+		cursor(HAND);
+		if (mouseIsPressed) {
+			strokeWeight(5);
+			// this is for natural smooth movment
+			x -= pmouseX - mouseX;
+			y -= pmouseY - mouseY;
+		} else {
+			strokeWeight(2);
+		}
+	} else {
+		noStroke();
+		cursor(ARROW);
+	}
+
+	ellipse(x, y, s, s);
+}
