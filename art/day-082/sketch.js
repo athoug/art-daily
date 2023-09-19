83638e62f85d56c9f574d9426ae1effcbc691901
Athoug <athoug.s@gmail.com>
2023-09-19 11:06:44 +0300

    tweening sketch

--- art/day-082/sketch.js
+++ art/day-082/sketch.js
@@ -3,8 +3,53 @@ const height = 400;
 const halfW = width / 2;
 const halfH = height / 2;
 
+let bgColor;
+let x = halfW;
+let y = halfH;
+let sz = 100;
+
+let targetWeight = 2;
+let currentWeight = 2;
+
+let targetAlpha = 0;
+let currentAlpha = 0;
+
 function setup() {
 	createCanvas(width, height);
+	colorMode(HSB);
+	smooth();
+	noStroke();
+
+	fill(random(255), 150, 200);
+	bgColor = color(random(255), 80, 255);
+	background(bgColor);
 }
 
-function draw() {}
+function draw() {
+	background(bgColor);
+
+	if (dist(x, y, mouseX, mouseY) < sz / 2) {
+		targetAlpha = 255;
+
+		if (mouseIsPressed) {
+			cursor('grab');
+			targetWeight = 10;
+			x = lerp(x, mouseX, 0.2);
+			y = lerp(y, mouseY, 0.2);
+		} else {
+			cursor(HAND);
+			targetWeight = 5;
+		}
+	} else {
+		cursor(ARROW);
+		targetAlpha = 0;
+	}
+
+	currentWeight = lerp(currentWeight, targetWeight, 0.2);
+	strokeWeight(currentWeight);
+
+	currentAlpha = lerp(currentAlpha, targetAlpha, 0.2);
+	stroke(255, currentAlpha);
+
+	ellipse(x, y, sz, sz);
+}
