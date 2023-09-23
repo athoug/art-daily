3bfafb3a0104963c1a1c094c2c5233242e78d921
Athoug <athoug.s@gmail.com>
2023-09-23 12:28:16 +0300

    verion 1 of audio

--- art/day-086/v1.js
+++ art/day-086/v1.js
@@ -3,8 +3,58 @@ const height = 400;
 const halfW = width / 2;
 const halfH = height / 2;
 
-function setup() {
-	createCanvas(width, height);
+let x = halfW;
+let y = 50;
+let dx = 5;
+let dy = 5;
+let s = 50;
+let bounce;
+
+function preload() {
+	soundFormats('mp3', 'ogg');
+	bounce = loadSound('./assets/bounce.mp3');
 }
 
-function draw() {}
+function setup() {
+	createCanvas(width, height);
+	fill(255, 0, 0);
+	smooth();
+	noStroke();
+
+	// bounce.start();
+	getAudioContext().resume();
+}
+
+function draw() {
+	background(255);
+	ellipse(x, y, s, s);
+
+	x += dx;
+	y += dy;
+
+	if (x > width) {
+		x = width;
+		dx = -dx;
+		bounce.play();
+	}
+
+	if (x < 0) {
+		x = 0;
+		dx = -dx;
+		bounce.play();
+	}
+
+	if (y > height) {
+		y = height;
+		dy = -dy;
+		bounce.play();
+	}
+
+	if (y < 0) {
+		y = 0;
+		dy = -dy;
+		bounce.play();
+	}
+
+	// bounce.stop();
+}
