a99efaa907894b0703ad48327e6e5d1f8c5bf42f
Athoug <athoug.s@gmail.com>
2023-09-28 07:50:25 +0300

    jitters

--- art/day-090/Bug.js
+++ art/day-090/Bug.js
@@ -8,17 +8,19 @@ class Bug {
 		this.x = x;
 		this.y = y;
 		this.c = c;
-		this.s = random(5, 20);
+		this.s = random(5, 30);
 	}
 
 	jitter() {
 		noStroke();
 		fill(this.c);
-		const targetX = random(-5, 5);
-		const targetY = random(-5, 5);
+		const targetX = random(-2, 2);
+		const targetY = random(-2, 2);
 
-		this.x = lerp(this.x, targetX, 0.2);
-		this.y = lerp(this.y, targetY, 0.2);
+		this.x += targetX;
+		this.y += targetY;
+
+		console.log(targetX, targetY);
 
 		ellipse(this.x, this.y, this.s, this.s);
 	}
--- art/day-090/sketch.js
+++ art/day-090/sketch.js
@@ -3,8 +3,26 @@ const height = 400;
 const halfW = width / 2;
 const halfH = height / 2;
 
+const bugs = [];
 function setup() {
 	createCanvas(width, height);
+	smooth();
 }
 
-function draw() {}
+function draw() {
+	background(255, 30);
+
+	for (let i = 0; i < bugs.length; i++) {
+		bugs[i].jitter();
+	}
+}
+
+function mouseClicked() {
+	const bug = new Bug(
+		mouseX,
+		mouseY,
+		color(random(255), random(255), random(255))
+	);
+	bugs.push(bug);
+	return false;
+}
