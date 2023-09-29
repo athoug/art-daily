d0f834c0a4da4678f942833d1976bf20caca0dd1
Athoug <athoug.s@gmail.com>
2023-09-29 11:03:25 +0300

    jittery circles

--- art/day-091-1/v1.js
+++ art/day-091-1/v1.js
@@ -3,8 +3,28 @@ const height = 400;
 const halfW = width / 2;
 const halfH = height / 2;
 
+const bugs = [];
+const maxBugs = 150;
+
 function setup() {
 	createCanvas(width, height);
+	smooth();
+	noStroke();
+	fill(255);
+
+	for (let i = 0; i < maxBugs; i++) {
+		const x = width / 2 + sin(i / 2) * 100;
+		const y = height / 2 + cos(i / 2) * 100;
+
+		const bug = new Bug(x, y, 0.05 + i / 1000);
+		bugs.push(bug);
+	}
 }
 
-function draw() {}
+function draw() {
+	background(150, 0, 0);
+
+	for (let i = 0; i < bugs.length; i++) {
+		bugs[i].live();
+	}
+}
