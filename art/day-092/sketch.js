06914e02120f95ca52f9b6608a83d181c3aafce7
Athoug <athoug.s@gmail.com>
2023-10-01 10:59:38 +0300

    Update sketch.js

--- art/day-092/sketch.js
+++ art/day-092/sketch.js
@@ -3,8 +3,20 @@ const height = 400;
 const halfW = width / 2;
 const halfH = height / 2;
 
+let data;
+function preload() {
+	data = loadBytes('./assets/image.png');
+}
+
 function setup() {
 	createCanvas(width, height);
+	colorMode(HSB);
+
+	for (let i = 0; i < width; i++) {
+		const myHue = data.bytes[i].toString(8);
+		stroke(myHue, 50, 255);
+		line(i, 0, i, height);
+	}
 }
 
 function draw() {}
