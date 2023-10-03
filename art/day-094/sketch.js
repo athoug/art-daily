0ecab2cfb9ca20f92359bbc8075863345bec9ae0
Athoug <athoug.s@gmail.com>
2023-10-03 09:58:35 +0300

    Update sketch.js

--- art/day-094/sketch.js
+++ art/day-094/sketch.js
@@ -18,3 +18,9 @@ function draw() {
 	image(capture, random(width), random(height), 320, 240);
 	// filter(INVERT);
 }
+
+/*
+	this comment helped me solve the permission issue in the dom 
+	https://stackoverflow.com/questions/59000581/google-chrome-domexception-permission-denied-by-system-for-navigator-mediadevic#:~:text=got%20the%20same%20error%20in%20a%20slightly%20different%20case%20(on%20Windows%2010%2C%20Chrome)%3A%20I%20was%20running%20a%20local%20server%20and%20accessed%20my%20own%20website%20from%20http%3A//127.0.0.1%3A5500/.%20The%20solution%20was%20to%20access%20it%20from%20http%3A//localhost%3A5500/%20instead.
+
+*/
