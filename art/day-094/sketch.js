e41b075a7e52f82c3fd64e298fb5a34c4e375b01
Athoug <athoug.s@gmail.com>
2023-10-03 09:57:51 +0300

    add sketch

--- art/day-094/sketch.js
+++ art/day-094/sketch.js
@@ -3,8 +3,18 @@ const height = 400;
 const halfW = width / 2;
 const halfH = height / 2;
 
+let capture;
+
 function setup() {
 	createCanvas(width, height);
+	capture = createCapture(VIDEO);
+	capture.size(320, 240);
+	capture.hide();
+	frameRate(10);
 }
 
-function draw() {}
+function draw() {
+	// background(255);
+	image(capture, random(width), random(height), 320, 240);
+	// filter(INVERT);
+}
