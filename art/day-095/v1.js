23d6736cb71f7ede419e5b0bcb270a6df6d6ba73
Athoug <athoug.s@gmail.com>
2023-10-04 06:32:12 +0300

    starter version

--- art/day-095/v1.js
+++ art/day-095/v1.js
@@ -3,8 +3,24 @@ const height = 400;
 const halfW = width / 2;
 const halfH = height / 2;
 
+const k = 1 / 2;
+
 function setup() {
 	createCanvas(width, height);
+	background('#129575');
+	colorMode(HSB);
+	smooth();
+	strokeWeight(0.01);
 }
 
-function draw() {}
+function draw() {
+	translate(halfW, halfH);
+	scale(halfW, halfH);
+
+	const t = frameCount / 20;
+	const x = cos(k * t) * sin(t);
+	const y = cos(k * t) * cos(t);
+
+	stroke(255);
+	line(0, 0, x, y);
+}
