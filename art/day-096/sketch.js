318ede53151ac269c64598846330ba2b0d4c3560
Athoug <athoug.s@gmail.com>
2023-10-05 09:41:26 +0300

    video

--- art/day-096/sketch.js
+++ art/day-096/sketch.js
@@ -3,8 +3,16 @@ const height = 400;
 const halfW = width / 2;
 const halfH = height / 2;
 
+let vid;
+
 function setup() {
-	createCanvas(width, height);
+	noCanvas();
+	vid = createVideo('./assets/movie.mov', loadVideo);
+
+	vid.size(450, 750);
 }
 
-function draw() {}
+function loadVideo() {
+	vid.loop();
+	vid.volume(1);
+}
