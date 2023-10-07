fd8d4825a95f6ce1f066955d4ab90c422a7d5021
Athoug <athoug.s@gmail.com>
2023-10-07 13:32:39 +0300

    filter funsies

--- art/day-098/v1.js
+++ art/day-098/v1.js
@@ -5,6 +5,19 @@ const halfH = height / 2;
 
 function setup() {
 	createCanvas(width, height);
+	fill(255);
+	stroke(255);
+	background(255);
+
+	// for (let i = 0; i < 20; i++) {
+	// 	line(0, random(height), width, random(height));
+	// }
 }
 
-function draw() {}
+function draw() {
+	stroke(random(255), random(255), random(255));
+	strokeWeight(random(5));
+
+	line(0, random(height), width, random(height));
+	filter(BLUR, 2);
+}
