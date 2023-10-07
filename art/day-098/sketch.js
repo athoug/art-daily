7faba386ef664e3785b38c840ba524df3e650af4
Athoug <athoug.s@gmail.com>
2023-10-07 13:43:27 +0300

    fun processing!

--- art/day-098/sketch.js
+++ art/day-098/sketch.js
@@ -5,6 +5,18 @@ const halfH = height / 2;
 
 function setup() {
 	createCanvas(width, height);
+	background(0);
+	stroke(255);
+	fill(255);
+	textSize(80);
 }
 
-function draw() {}
+function draw() {
+	if (mouseIsPressed) {
+		fill(keyIsPressed ? 255 : 0);
+		text(char(int(random(65, 90))), mouseX - 30, mouseY + 40);
+	}
+
+	filter(BLUR, 3);
+	filter(THRESHOLD);
+}
