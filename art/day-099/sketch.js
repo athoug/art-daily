09850293b6049f645f348635988e3bc82e721f6a
Athoug <athoug.s@gmail.com>
2023-10-08 10:08:28 +0300

    a sketch

--- art/day-099/sketch.js
+++ art/day-099/sketch.js
@@ -5,6 +5,20 @@ const halfH = height / 2;
 
 function setup() {
 	createCanvas(width, height);
+	noStroke();
+	rectMode(CENTER);
 }
 
-function draw() {}
+function draw() {
+	background(0);
+	fill(255, 255, 255);
+
+	for (let y = 48; y < height - 43; y += 43) {
+		for (let x = 48; x < width - 43; x += 27) {
+			resetMatrix();
+			translate(x, y);
+			rotate(TWO_PI * noise(x, y, frameCount / 70));
+			rect(0, 0, 8, 27);
+		}
+	}
+}
