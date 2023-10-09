e6d17ae50d33725c60e0a530a20a5d61d1065e3d
Athoug <athoug.s@gmail.com>
2023-10-09 09:46:45 +0300

    recursion

--- art/day-100/sketch.js
+++ art/day-100/sketch.js
@@ -3,8 +3,51 @@ const height = 400;
 const halfW = width / 2;
 const halfH = height / 2;
 
+let x, y, sz;
 function setup() {
 	createCanvas(width, height);
+	background('#605130');
+	noStroke();
+	frameRate(1);
+	drawMe();
 }
 
-function draw() {}
+function drawMeFacile(x, y, sz) {
+	let a, nx, ny;
+
+	const from = color('#B5D333');
+	const to = color('#8F683F');
+
+	fill(lerpColor(from, to, random(1)), 0.5);
+	ellipse(x, y, sz, sz);
+
+	if (sz > 1) {
+		a = random(TWO_PI);
+		nx = x + (sz / 2) * sin(a);
+		ny = y + (sz / 2) * cos(a);
+		drawMeFacile(nx, ny, sz / 2);
+
+		a = random(TWO_PI);
+		nx = x + (sz / 2) * sin(a);
+		ny = y + (sz / 2) * cos(a);
+		drawMeFacile(nx, ny, sz / 2);
+
+		a = random(TWO_PI);
+		nx = x + (sz / 2) * sin(a);
+		ny = y + (sz / 2) * cos(a);
+		drawMeFacile(nx, ny, sz / 2);
+	}
+}
+
+function draw() {
+	drawMe();
+}
+
+function drawMe() {
+	background('#605130');
+	x = halfW;
+	y = halfH;
+	sz = 200;
+
+	drawMeFacile(x, y, sz);
+}
