a25ca1aaa835e6fd85fd99f0189f9f0edfc71a7c
Athoug <athoug.s@gmail.com>
2023-10-10 10:45:31 +0300

    practice 3

--- art/day-101/sketch.js
+++ art/day-101/sketch.js
@@ -5,6 +5,19 @@ const halfH = height / 2;
 
 function setup() {
 	createCanvas(width, height);
+	noStroke();
 }
 
-function draw() {}
+function draw() {
+	background(0);
+	resetMatrix();
+	translate(halfW, halfH);
+	rectMode(CENTER);
+
+	for (let i = 0; i < 32; i++) {
+		fill((i % 2) * 250);
+		rect(0, 0, 500, 500);
+		rotate(frameCount / 100);
+		scale(0.915);
+	}
+}
