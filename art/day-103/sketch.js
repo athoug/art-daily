c16d9a3da943a82008d4ad294114662b5ad72249
Athoug <athoug.s@gmail.com>
2023-10-12 10:23:42 +0300

    sketch

--- art/day-103/sketch.js
+++ art/day-103/sketch.js
@@ -3,8 +3,27 @@ const height = 400;
 const halfW = width / 2;
 const halfH = height / 2;
 
+const pg = [];
+const frames = 20;
+
 function setup() {
 	createCanvas(width, height);
+
+	for (let i = 0; i < frames; i++) {
+		pg[i] = createGraphics(width, height);
+		pg[i].clear(); // make graphic clear
+		pg[i].background(30);
+		pg[i].stroke(255);
+		pg[i].strokeWeight(3);
+	}
 }
 
-function draw() {}
+function draw() {
+	const currFrame = frameCount % frames; // 0 ... 19
+
+	if (mouseIsPressed) {
+		pg[currFrame].line(mouseX, mouseY, pmouseX, pmouseY);
+	}
+
+	image(pg[currFrame], 0, 0);
+}
