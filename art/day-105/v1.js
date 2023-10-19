9e89eadb5714ca3fc13f1ec022ebb628dae272eb
Athoug <athoug.s@gmail.com>
2023-10-19 07:48:20 +0300

    practice

--- art/day-105/v1.js
+++ art/day-105/v1.js
@@ -3,8 +3,27 @@ const height = 400;
 const halfW = width / 2;
 const halfH = height / 2;
 
+let a;
+let t = 0;
+const howMany = 30;
+
+function preload() {
+	a = loadImage('./assets/a.svg');
+}
+
 function setup() {
 	createCanvas(width, height);
 }
 
-function draw() {}
+function draw() {
+	background(31);
+	for (let i = 0; i < howMany; i++) {
+		push();
+		translate(halfW, halfH);
+		rotate(i);
+		image(a, (i % 4) * 25 * noise(i, t), (i % 5) * 30 * noise(i, t * 1.7));
+		pop();
+	}
+
+	t += 0.03;
+}
