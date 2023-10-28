3d78677d9aed85dd1fadd8d8895f15c107ef53d4
Athoug <athoug.s@gmail.com>
2023-10-28 11:53:44 +0300

    starter

--- art/day-106/v1.js
+++ art/day-106/v1.js
@@ -3,8 +3,24 @@ const height = 400;
 const halfW = width / 2;
 const halfH = height / 2;
 
+let cam;
+
 function setup() {
 	createCanvas(width, height);
+
+	cam = createCapture(VIDEO);
+	cam.size(width, height);
+	cam.hide();
 }
 
-function draw() {}
+function draw() {
+	background(255);
+
+	let maxBri = 0;
+	let theBrightestPixel = 1;
+	cam.loadPixels();
+	for (let i = 0; i < cam.pixels.length; i++) {
+		console.log(i);
+	}
+	image(cam, 0, 0, width, height);
+}
