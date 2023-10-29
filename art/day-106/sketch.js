4cb73be0626e2060435d14aed617c13a72d32ccb
Athoug <athoug.s@gmail.com>
2023-10-29 15:28:09 +0300

    mirror pixels

--- art/day-106/sketch.js
+++ art/day-106/sketch.js
@@ -1,10 +1,42 @@
-const width = 400;
-const height = 400;
+const width = 640;
+const height = 480;
 const halfW = width / 2;
 const halfH = height / 2;
 
+let cam;
+let vScale = 16;
+
 function setup() {
 	createCanvas(width, height);
+	pixelDensity(1);
+
+	cam = createCapture(VIDEO);
+	cam.size(width / vScale, height / vScale);
+	cam.hide();
 }
 
-function draw() {}
+function draw() {
+	background(51);
+
+	cam.loadPixels();
+	loadPixels();
+
+	for (let y = 0; y < cam.height; y++) {
+		for (let x = 0; x < cam.width; x++) {
+			const index = (cam.width - x + 1 + y * cam.width) * 4;
+
+			const r = cam.pixels[index];
+			const g = cam.pixels[index + 1];
+			const b = cam.pixels[index + 2];
+
+			const bright = (r + b + b) / 3;
+
+			const w = map(bright, 0, 255, 0, vScale);
+
+			noStroke();
+			fill(255);
+			rectMode(CENTER);
+			ellipse(x * vScale, y * vScale, w, w);
+		}
+	}
+}
--- art/day-106/v1.js
+++ art/day-106/v1.js
@@ -1,33 +1,38 @@
-const width = 400;
-const height = 400;
+const width = 640;
+const height = 480;
 const halfW = width / 2;
 const halfH = height / 2;
 
 let cam;
+let vScale = 16;
 
 function setup() {
 	createCanvas(width, height);
 	pixelDensity(1);
+
+	cam = createCapture(VIDEO);
+	cam.size(width / vScale, height / vScale);
+	cam.hide();
 }
 
 function draw() {
 	background(51);
 
-	let maxBri = 0;
-	let theBrightestPixel = 1;
-
+	cam.loadPixels();
 	loadPixels();
 
-	for (let y = 0; y < height; y++) {
-		for (let x = 0; x < width; x++) {
-			const index = (x + y * width) * 4;
+	for (let y = 0; y < cam.height; y++) {
+		for (let x = 0; x < cam.width; x++) {
+			const index = (cam.width - x + 1 + y * cam.width) * 4;
 
-			pixels[index] = x;
-			pixels[index + 1] = random(255);
-			pixels[index + 2] = y;
-			pixels[index + 3] = 255;
+			const r = cam.pixels[index];
+			const g = cam.pixels[index + 1];
+			const b = cam.pixels[index + 2];
+
+			const bright = (r + b + b) / 3;
+
+			fill(bright);
+			rect(x * vScale, y * vScale, vScale, vScale);
 		}
 	}
-
-	updatePixels();
 }
