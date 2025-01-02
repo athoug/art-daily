4e0f1569ba31cac6430c735e5b02ae523429efb6
Athoug <athoug.s@gmail.com>
2025-01-02 12:02:25 +0300

    movment with come dynamics

--- art/day-111/cell.js
+++ art/day-111/cell.js
@@ -7,17 +7,20 @@ class Cell {
 		this.y0 = y0;
 	}
 
-	update() {
+	update(angle = null) {
 		this.x = this.r * cos(this.angle);
 		this.y = this.r * sin(this.angle);
-
-		this.angle += 0.05;
+		if (angle) {
+			this.r = angle;
+		}
+		this.angle += 0.02;
 	}
 
-	display() {
+	display(c = 0) {
 		// ellipse(this.x0, this.y0, this.r*2, this.r*2)
 		// line(this.x0, this.y0, this.x0+this.x, this.y0+this.y);
-		fill(0);
+		fill(c);
+		noStroke();
 		ellipse(this.x0 + this.x, this.y0 + this.y, this.size, this.size);
 	}
 }
---
+++ art/day-111/sketch-2.js
@@ -0,0 +1,40 @@
+const grid = [];
+const cols = 15;
+const rows = 15;
+const loc = 100;
+
+function setup() {
+	createCanvas(400, 400);
+	// we need some grid spacing
+	const rowSize = height / rows;
+	const colSize = width / cols;
+
+	const halfCol = colSize / 2;
+	const halfRow = rowSize / 2;
+
+	// populating the grid with the cell object
+	for (let i = 0; i < cols; i++) {
+		grid[i] = [];
+		for (let j = 0; j < rows; j++) {
+			grid[i][j] = new Cell(
+				halfCol + i * colSize,
+				halfRow + j * rowSize,
+				halfRow,
+				i * loc + j * loc
+			);
+		}
+	}
+}
+
+function draw() {
+	background('#721817');
+
+	// we also need another loop to display and update them
+	// populating the grid with the cell object
+	for (let i = 0; i < cols; i++) {
+		for (let j = 0; j < rows; j++) {
+			grid[i][j].update(mouseX * 0.1 + mouseY * 0.2);
+			grid[i][j].display('#fa9f42');
+		}
+	}
+}
