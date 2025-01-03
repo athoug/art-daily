d9204d550f96f62a7a75c36fe68d057c8df1e1af
Athoug <athoug.s@gmail.com>
2025-01-03 13:58:07 +0300

    add direction

---
+++ art/day-112/circle-2.js
@@ -0,0 +1,27 @@
+class Circle {
+	constructor(angle, c) {
+		this.r = 100;
+		this.angle = angle;
+		this.size = 100;
+		this.direction = -1;
+		this.c = c;
+	}
+
+	update() {
+		this.x = this.r * cos(this.angle);
+		this.y = this.r * sin(this.angle);
+
+		if (this.r < 0 || this.r > 100) {
+			this.direction *= -1;
+		}
+
+		this.r += this.direction;
+		this.angle += 0.01;
+	}
+
+	display() {
+		fill(this.c);
+		ellipse(this.x, this.y, this.size, this.size);
+		line(0, 0, this.x, this.y);
+	}
+}
